05223aba4cf53ec7e0f226987e0ee594c4c0edc5
G-tyan <pekotyan2000@gmail.com>
2020-05-05 17:07:30 +0900

    コメントアウト

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//以下(class MailActivity)がアクティビティ生成時に呼ばれるコールバックメソッド
+
 class MainActivity : AppCompatActivity() {
+
+    //onCreate()がコールバックメソッド、初期設定を行う
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
